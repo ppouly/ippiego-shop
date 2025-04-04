be1e1b28e21d64d741b3122aac2664b9e3c3b9aa
IppieGoDev <sujungbaik@gmail.com>
2025-04-04 21:25:48 +0900

    메인배너수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,11 +7,34 @@ import BottomNav from "@/components/BottomNav";
 
 export const metadata = {
   title: "IPPIEGO | 빈티지 키즈 셀렉트샵",
-  description: "자유롭고 감각적인 빈티지 키즈 패션, IPPIEGO에서 만나보세요.",
-  metadataBase: new URL("https://your-deploy-url.vercel.app"),
+  description: "빈티지 수입 아동복 마켓, 자유롭고 감성적인 아이들을 위한 IPPIEGO.",
+  keywords: [
+    "키즈 패션", "빈티지 아동복", "수입 아동의류", "보보쇼즈", "미니로디니", "루이스미샤", "아폴리니", "타오", "수입 아동복", "IPPiEGO", "ippiego.shop",
+  ],
+  authors: [{ name: "IPPIEGO", url: "https://ippiego.shop" }],
+  creator: "IPPIEGO",
+  metadataBase: new URL("https://ippiego.shop"),
   openGraph: {
-    title: "IPPIEGO",
-    images: ["/og-image.png"],
+    title: "IPPIEGO | 빈티지 키즈 셀렉트샵",
+    description: "자유롭고 감성적인 아이들을 위한 빈티지 키즈 셀렉트샵, IPPIEGO.",
+    url: "https://ippiego.shop",
+    siteName: "IPPIEGO",
+    images: [
+      {
+        url: "/og-image.png", // public 폴더에 넣어주세요
+        width: 1200,
+        height: 630,
+        alt: "IPPIEGO 오픈그래프 이미지",
+      },
+    ],
+    locale: "ko_KR",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "IPPIEGO | 빈티지 키즈 셀렉트샵",
+    description: "자유롭고 감성적인 아이들을 위한 빈티지 키즈 셀렉트샵",
+    images: ["/og-image.jpg"],
   },
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,16 +34,20 @@ const products = [
 
 const banners = [
   {
-    image: "/banner1.jpg",
+    image: "/banner1.png",
     text: "Dress Up, Run Wild, Go Ippie!",
   },
   {
-    image: "/banner2.jpg",
-    text: "Vintage Vibes, Modern Fun",
+    image: "/banner2.png",
+    //text: "Vintage Vibes, Modern Fun",
   },
   {
-    image: "/banner3.jpg",
-    text: "Bold Colors, Big Adventures",
+    image: "/banner3.png",
+    //text: "Bold Colors, Big Adventures",
+  },
+  {
+    image: "/banner4.png",
+    //text: "Bold Colors, Big Adventures",
   },
 ];
 
@@ -70,8 +74,10 @@ function MainBannerSlider() {
       {banners.map((banner, index) => (
         <div
           key={index}
-          className="keen-slider__slide relative h-full w-full bg-cover bg-center"
-          style={{ backgroundImage: `url(${banner.image})` }}
+          className="keen-slider__slide relative h-full w-full bg-contain bg-center bg-no-repeat"
+          style={{ backgroundImage: `url(${banner.image})`, 
+                   backgroundColor: "#fff5e0", // 예: 크림색 (브랜드 톤) 
+                  }}
         >
           <motion.div
             initial={{ opacity: 0, y: 30 }}
