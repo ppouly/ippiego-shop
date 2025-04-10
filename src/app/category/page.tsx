597da4dc8b87afd4c0509a5c832347f52610723d
IppieGoDev <sujungbaik@gmail.com>
2025-04-10 15:56:15 +0900

    카테고리탭수정

--- src/app/category/page.tsx
+++ src/app/category/page.tsx
@@ -1,6 +1,5 @@
-import CategoryPage from "@/components/CategoryPage";
-
 export const dynamic = "force-dynamic"; // <- 이것만 추가하면 끝!
+import CategoryPage from "@/components/CategoryPage";
 
 export default function CategoryRoute() {
   return <CategoryPage />;
