9849d875592affb9f5747ee77dfe1abfad1363fe
IppieGoDev <sujungbaik@gmail.com>
2025-04-17 23:21:21 +0900

    인증방식구현-테스트용전번확인 코드로 수정

--- src/app/api/send-code/route.ts
+++ src/app/api/send-code/route.ts
@@ -1,16 +1,16 @@
 //import { NextResponse } from "next/server";
-//import { supabase } from "@/lib/supabase";
+import { supabase } from "@/lib/supabase";
 
 export async function POST(req: Request) {
   const { phone } = await req.json();
   const code = Math.floor(100000 + Math.random() * 900000).toString();
 
     // 실제로는 메시지 전송 로직이 여기에 들어가야 함
-//   await supabase.from("order_verifications").insert({
-//     phone,
-//     code,
-//     verified: false,
-//   });
+  await supabase.from("order_verifications").insert({
+    phone,
+    code,
+    verified: false,
+  });
 
   console.log(`📨 인증번호 전송됨: ${code} → ${phone}`);
 //   return NextResponse.json({ success: true });
