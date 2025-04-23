271fc79446ce357594a198b3b7bfe94bb4959413
IppieGoDev <sujungbaik@gmail.com>
2025-04-23 18:49:01 +0900

    네이버연동

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,25 +8,25 @@ import BottomNav from "@/components/BottomNav";
 import ThemeInit from "@/components/ThemeInit"; // 👈 다크모드 감지용 클라이언트 컴포넌트
 
 export const metadata = {
-  title: "IPPIEGO | 빈티지 키즈 셀렉트샵",
-  description: "빈티지 수입 아동복 마켓, 자유롭고 감성적인 아이들을 위한 IPPIEGO.",
+  title: "입히고 IPPIEGO | 중고 수입 유아동복 리세일 마켓",
+  description: "멋은 그대로, 부담은 덜게. 프리미엄 중고 아동복을 입히고에서 만나보세요. IPPIE GO!",
   keywords: [
-    "키즈 패션", "빈티지 아동복", "수입 아동의류", "보보쇼즈", "미니로디니", "루이스미샤", "아폴리니", "타오", "수입 아동복", "IPPiEGO", "ippiego.shop",
+    "입히고","키즈 패션", "빈티지 아동복", "수입 아동의류", "보보쇼즈", "미니로디니", "루이스미샤", "아폴리니", "타오","던스", "수입 아동복", "IPPiEGO", "ippiego.shop",
   ],
   authors: [{ name: "IPPIEGO", url: "https://ippiego.shop" }],
   creator: "IPPIEGO",
   metadataBase: new URL("https://ippiego.shop"),
   openGraph: {
     title: "IPPIEGO | 중고 수입 유아동복 리세일샵",
-    description: "빈티지 수입 유아동복 마켓, 자유롭고 감성적인 아이들을 위한 IPPIEGO.",
+    description: "중고 수입 유아동복 리세일 마켓, 자유롭고 감성적인 아이들을 위한 IPPIEGO.",
     url: "https://ippiego.shop",
     siteName: "IPPIEGO",
     images: [
       {
         url: "/og-image.jpg",
-        width: 1200,
-        height: 630,
-        alt: "IPPIEGO 오픈그래프 이미지",
+        width: 800,
+        height: 600,
+        alt: "IPPIEGO 대표 이미지",
       },
     ],
     locale: "ko_KR",
@@ -38,6 +38,10 @@ export const metadata = {
     description: "자유롭고 감성적인 아이들을 위한 빈티지 유아동복 리세일샵",
     images: ["/og-image.jpg"],
   },
+  // 여기 ↓ HTML 메타 태그 직접 삽입
+  other: {
+    'naver-site-verification': '77e806aef27f37e2db1492c696b6c355a9674d99',
+  },  
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
