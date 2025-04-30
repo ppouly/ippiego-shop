3703ecc0e90889fc4ea384f4a47282dd774c1886
IppieGoDev <sujungbaik@gmail.com>
2025-04-30 19:41:20 +0900

    메인페이지 SEO최적화1차

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata = {
   title: "입히고 IPPIEGO | 중고 수입 유아동복 리세일 마켓",
   description: "멋은 그대로, 부담은 덜게. 프리미엄 중고 아동복을 입히고에서 만나보세요. IPPIE GO!",
   keywords: [
-    "입히고","키즈 패션", "빈티지 아동복", "수입 아동의류", "보보쇼즈", "미니로디니", "루이스미샤", "아폴리니", "타오","던스", "수입 아동복", "IPPiEGO", "ippiego.shop",
+    "입히고","키즈 패션", "빈티지 아동복", "수입 아동의류", "보보쇼즈", "보보쇼즈 딸기", "미니로디니", "루이스미샤", "아폴리니", "타오","던스", "던스 순무", "수입 아동복", "IPPiEGO", "ippiego.shop",
   ],
   authors: [{ name: "IPPIEGO", url: "https://ippiego.shop" }],
   creator: "IPPIEGO",
@@ -34,7 +34,7 @@ export const metadata = {
   },
   twitter: {
     card: "summary_large_image",
-    title: "입히고 | 중고 수입 유아동복 리세일샵",
+    title: "입히고 | 환불보장 엄선된 중고 수입 유아동복 리세일샵",
     description: "자유롭고 감성적인 아이들을 위한 빈티지 유아동복 리세일샵",
     images: ["/og-image.jpg"],
   },
