548ed7c164045d7e303d12b5b799d49dd8ccd32e
IppieGoDev <sujungbaik@gmail.com>
2025-05-01 10:57:59 +0900

    입히고샵 네이밍

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,8 +8,8 @@ import BottomNav from "@/components/BottomNav";
 import ThemeInit from "@/components/ThemeInit"; // 👈 다크모드 감지용 클라이언트 컴포넌트
 
 export const metadata = {
-  title: "입히고샵 | 환불보장, 중고 수입 유아동복 리세일샵 IPPIE Go!",
-  description: "보보쇼즈, 미니로디니, 던스, 루이스미샤 등 중고 수입 유아동복 리세일 마켓, 멋은 그대로, 부담은 덜게, 환불가능하니까, 먼저 입혀보세요",
+  title: "입히고샵 | 환불보장, 수입 유아동복 중고 마켓 IPPIE Go!",
+  description: "보보쇼즈, 미니로디니 등 수입 유아동복 중고 마켓, 멋은 그대로, 부담은 덜게, 입히고샵은 환불가능하니까, 먼저 입혀보세요",
   keywords: [
    '보보쇼즈 중고', '미니로디니 중고','던스 순무','보보쇼즈 딸기','아폴리나 중고','루이스미샤 중고','타오 중고', '중고 아동복', '입히고', '수입 아동복',"입히고",
   ],
@@ -17,8 +17,8 @@ export const metadata = {
   creator: "입히고",
   metadataBase: new URL("https://ippiego.shop"),
   openGraph: {
-    title: "입히고샵 | 환불보장, 중고 수입 유아동복 리세일샵 IPPIE Go!",
-    description: "보보쇼즈, 미니로디니, 던스, 루이스미샤 등 중고 수입 유아동복 리세일 마켓, 멋은 그대로, 부담은 덜게, 환불가능하니까, 먼저 입혀보세요",
+    title: "입히고샵 | 환불보장, 수입 유아동복 중고 마켓 IPPIE Go!",
+    description: "보보쇼즈, 미니로디니 등 수입 유아동복 중고 마켓, 멋은 그대로, 부담은 덜게, 입히고샵은 환불가능하니까, 먼저 입혀보세요",
     url: "https://ippiego.shop",
     siteName: "입히고샵",
     images: [
@@ -34,8 +34,8 @@ export const metadata = {
   },
   twitter: {
     card: "summary_large_image",
-    title: "입히고샵 | 환불보장, 중고 수입 유아동복 리세일샵 IPPIE Go!",
-    description: "자유롭고 감성적인 아이들을 위한 빈티지 유아동복 리세일샵",
+    title: "입히고샵 | 환불보장, 수입 유아동복 중고 마켓 IPPIE Go!",
+    description: "자유롭고 감성적인 아이들을 위한 유아동복 중고 마켓. 멋은 그대로, 부담은 덜게",
     images: ["/og-image.jpg"],
   },
   // 여기 ↓ HTML 메타 태그 직접 삽입
