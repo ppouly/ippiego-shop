572a55f79319daa7b3560af1232074f1d94db553
IppieGoDev <sujungbaik@gmail.com>
2025-05-18 17:30:06 +0900

    배너 다시보지않기 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -146,8 +146,7 @@ export default function Home() {
 
   const REST_API_KEY = process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY!;
   const REDIRECT_URI = process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI!;
-  const kakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=code`;
-
+  
       
   return (
     <div className="p-4">
